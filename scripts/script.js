918a133ecc7e7cdc29b3f89a916d4d3aba15ba96
sifat2626 <ashrafulsifat26@gmail.com>
2024-02-19 17:43:34 +0600

    functionality completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,33 @@
 let availableSeatnum;
+let discountRate;
 let discount;
 let grandTotal;
+let couponSuccess;
 let reservedSeats = [];
 const ticketPrice = 550;
 let totalPrice;
 const coupons = [
   {
     code: "NEW15",
-    discount: 15,
+    discountRate: 15,
   },
   {
     code: "Couple 20",
-    discount: 20,
+    discountRate: 20,
   },
 ];
+
+const successModal = document.getElementById("successModal");
+
+const nextButton = document.getElementById("next");
+const nameInput = document.getElementById("name");
+const phoneInput = document.getElementById("phone");
+const emailInput = document.getElementById("email");
+let nameValue;
+let phoneValue;
+let emailValue;
+
+const inputForm = document.getElementById("input-form");
 
 const couponInput = document.getElementById("coupon-input");
 let couponValue;
@@ -21,7 +35,13 @@ const couponButton = document.getElementById("coupon-button");
 
 // console.log(coupons[0]);
 
+const hiddenTicketSection = document.getElementById("hidden-ticket-section");
+const hiddenTotalSection = document.getElementById("hidden-total-section");
+const hiddenCouponSection = document.getElementById("hidden-coupon-section");
+const hiddenGrandTotal = document.getElementById("hidden-grand-total");
+
 const totalContent = document.querySelector(".total");
+const discountContent = document.getElementById("hidden-discount");
 const grandTotalContent = document.querySelector(".grand-total");
 // console.log(totalContents);
 
@@ -38,26 +58,40 @@ availableSeatnum = availableSeats.length;
 couponButton.addEventListener("click", function (e) {
   e.preventDefault();
   couponValue = couponInput.value;
+
   //checking coupon value
   coupons.forEach((coupon) => {
-    if (coupon.code.toLowerCase() === couponValue.toLowerCase()) {
-      console.log(couponValue);
-      // calculating grand total
-      console.log(totalPrice);
-      discount = coupon.discount;
-      grandTotal = totalPrice - (totalPrice * discount) / 100;
+    if (coupon.code === couponValue) {
+      hiddenGrandTotal.classList.remove("hidden");
+      hiddenCouponSection.classList.add("hidden");
+      couponSuccess = true;
+      discountRate = coupon.discountRate;
+      discount = (totalPrice * discountRate) / 100;
+      discountContent.textContent = `- BDT ${discount}`;
+      grandTotal = totalPrice - discount;
       grandTotalContent.textContent = `BDT ${grandTotal}`;
     }
   });
+
+  if (!couponSuccess) {
+    alert("Sorry! wrong coupon");
+    // calculating grand total
+  }
 });
 
 availableSeats.forEach((seat, i) => {
   seat.addEventListener("click", function () {
-    if (reservedSeats.length < 2) {
-      reservedSeats.push(seat);
-      seat.classList.add("bg-green-500");
+    hiddenTicketSection.classList.remove("hidden");
+    hiddenTotalSection.classList.remove("hidden");
+    if (reservedSeats.length < 4 && !reservedSeats.includes(seat.id)) {
+      reservedSeats.push(seat.id);
+
+      seat.classList.add("bg-[#1DD100]");
+      seat.classList.remove("opacity-50");
       availableSeatnum--;
-      availableSeatText.textContent = `${availableSeatnum} seats left`;
+      availableSeatText.textContent = `${availableSeatnum} ${
+        availableSeatnum === 1 ? `seat` : `seats`
+      } left`;
 
       // Append the new line of HTML to the seats container
       seatsContainer.innerHTML += `
@@ -80,6 +114,52 @@ availableSeats.forEach((seat, i) => {
 
       totalContent.textContent = `BDT ${totalPrice}`;
       seatNumText.textContent = `${reservedSeats.length}`;
+
+      //next button enabling
+      checkInputs();
+
+      // enabling coupon button
+
+      if (reservedSeats.length === 4) {
+        hiddenCouponSection.classList.remove("hidden");
+        couponButton.removeAttribute("disabled");
+      }
     }
   });
 });
+
+// Add event listeners to input fields
+nameInput.addEventListener("input", checkInputs);
+phoneInput.addEventListener("input", checkInputs);
+emailInput.addEventListener("input", checkInputs);
+
+function checkInputs() {
+  var nameValue = nameInput.value.trim();
+  var phoneValue = phoneInput.value.trim();
+  var emailValue = emailInput.value.trim();
+
+  // Check if all input fields are filled
+  if (nameValue !== "" && phoneValue !== "" && reservedSeats.length) {
+    // Enable the next button
+    nextButton.disabled = false;
+  } else {
+    // Disable the next button
+    nextButton.disabled = true;
+  }
+}
+
+// ///////////////MODAL//////////////
+////////////////////////////////////
+///////////////////////////////////
+
+function openModal() {
+  var modal = document.getElementById("my_modal_5");
+  modal.showModal(); // Show the modal
+  // Customize modal styles here
+  modal.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Example customization
+}
+
+function closeModal() {
+  var modal = document.getElementById("my_modal_5");
+  modal.close(); // Close the modal
+}
